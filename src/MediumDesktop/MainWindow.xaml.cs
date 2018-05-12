34e21136c4dd7588b65a2cc158d709a17aabf42a
Dmitriy <dima_nikishin_84@mail.ru>
2018-05-12 17:50:00 +0300

    Revert "Merge branch 'dev'"
    This reverts commit 01e5f83e371928b0e84c00c3d96eaf6205e9c3b4.

--- src/MediumDesktop/MainWindow.xaml.cs
+++ src/MediumDesktop/MainWindow.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System.IO;
-using System.Net;
-using System.Net.Http;
-using System.Windows;
-
-namespace MediumDesktop
+﻿namespace MediumDesktop
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -12,29 +7,6 @@ namespace MediumDesktop
     {
         public MainWindow()
         {
-            var clientId = "ce250fa7c114";
-            var clientSecret = "76880f31a925708f1f04bc522c0c88b3e395edcb";
-
-
-            var client = new Medium.OAuthClient(clientId, clientSecret);
-
-            var url = client.GetAuthorizeUrl(
-                "secretstate",
-                "https://yoursite.com/callback/medium",
-                new[]
-                {
-                    Medium.Authentication.Scope.BasicProfile
-                });
-
-            var req = WebRequest.Create(url);
-
-            var resp = req.GetResponse();
-
-            client.GetAccessToken("","https://yoursite.com/callback/medium");
-
-
-           
-
             InitializeComponent();
         }
     }
