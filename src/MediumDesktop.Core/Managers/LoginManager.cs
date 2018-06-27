a5cee22347a501ef9e84f45047dd368255d42aca
Dmitriy <dima_nikishin_84@mail.ru>
2018-06-27 20:44:43 +0300

    fixes

---
+++ src/MediumDesktop.Core/Managers/LoginManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using MediumDesktop.Core.Managers.Interfaces;
+
+namespace MediumDesktop.Core.Managers
+{
+    public sealed class LoginManager : ILoginManager
+    {
+        public async Task LoginAsync(string username, string password)
+        {
+        }
+    }
+}
---
+++ src/MediumDesktop/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using DryIoc;
+using DryIoc.MefAttributedModel;
+using MediumDesktop.Core.Extensions;
+
+namespace MediumDesktop
+{   
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public sealed partial class App
+    {
+        private readonly IContainer _container = new Container().WithMefAttributedModel();
+
+        public App()
+        {
+            InitializeComponent();
+
+            _container.RegisterExports(new[] { typeof(App).GetAssembly() });
+
+            _container.RegisterShared();
+        }
+    }
+}
