89ac8a066ab3e2086a1bac80d73ea193ce11f553
Dmitriy <dima_nikishin_84@mail.ru>
2018-06-30 00:37:41 +0300

    clear code

--- src/MediumDesktop.Core/MediumAPI/ApiController.cs
+++ src/MediumDesktop.Core/MediumAPI/ApiController.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Net;
-using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using DryIocAttributes;
