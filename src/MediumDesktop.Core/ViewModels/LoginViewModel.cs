3fa93f6dbb36bbd136a86b704724409275a255ec
Dmitriy <dima_nikishin_84@mail.ru>
2018-06-29 15:06:07 +0300

    remove viewModel locator

--- src/MediumDesktop.Core/ViewModels/LoginViewModel.cs
+++ src/MediumDesktop.Core/ViewModels/LoginViewModel.cs
@@ -9,18 +9,14 @@ namespace MediumDesktop.Core.ViewModels
     [Reuse(ReuseType.Transient)]
     [ExportEx(typeof(LoginViewModel))]
     public sealed class LoginViewModel
-    {   
-        private readonly ILoginManager _loginManager;
-        private readonly INavigationService _navigationService;
-
-        public LoginViewModel(ILoginManager loginManager, INavigationService navigationService)
+    {
+        public LoginViewModel(
+            ILoginManager loginManager, 
+            INavigationService navigationService)
         {
-            _loginManager = loginManager;
-            _navigationService = navigationService;
-
             LoginCommand = ReactiveCommand.CreateFromTask(async () =>
             {
-                await _navigationService.NavigateAsync<MainPageViewModel>();
+                await loginManager.LoginAsync(Username, Password);
             });
         }
 
