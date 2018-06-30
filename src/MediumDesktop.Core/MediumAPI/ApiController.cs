53294b4fa002abe59c7433c06e5cd76adf75f976
Dmitriy <dima_nikishin_84@mail.ru>
2018-06-30 14:01:14 +0300

    refactoring

--- src/MediumDesktop.Core/MediumAPI/ApiController.cs
+++ src/MediumDesktop.Core/MediumAPI/ApiController.cs
@@ -1,10 +1,7 @@
-﻿using System.IO;
-using System.Net;
-using System.Text;
+﻿using System.Net;
 using System.Threading.Tasks;
 using DryIocAttributes;
-using Medium.Authentication;
-using Newtonsoft.Json;
+using MediumSDK.WPF.Domain;
 
 namespace MediumDesktop.Core.MediumAPI
 {
@@ -12,17 +9,18 @@ namespace MediumDesktop.Core.MediumAPI
     [ExportEx(typeof(IApiController))]
     public sealed class ApiController : IApiController
     {
-        private string _clientId = "ce250fa7c114";  
+        private string _clientId = "ce250fa7c114";
         private string _clientSecret = "76880f31a925708f1f04bc522c0c88b3e395edcb";
         private readonly string _redirectUrl = $"http://{IPAddress.Loopback}:{3000}/";
         private Token _accessToken;
 
         public async Task<bool> AuthorizateAsync()
         {
-            var code = await GetAuthCode();
+            var oAuthClient = new OauthClient(_clientId, _clientSecret, _redirectUrl, "text");
 
-            var accessToken = await GetToken(code);
+            var code = await oAuthClient.GetAuthCode();
 
+            var accessToken = await oAuthClient.GetToken(code);
             _accessToken = accessToken;
 
             return accessToken.AccessToken != null;
@@ -30,74 +28,6 @@ namespace MediumDesktop.Core.MediumAPI
 
         public async Task RefreshTokenAsync()
         {
-        }
-
-        private async Task<string> GetAuthCode()
-        {
-            var state = "text";
-            var http = new HttpListener();
-            http.Prefixes.Add(_redirectUrl);
-            http.Start();
-
-            var url =
-                $"https://medium.com/m/oauth/authorize?client_id={_clientId}&scope=basicProfile,publishPost&state={state}&response_type=code&redirect_uri={System.Uri.EscapeDataString(_redirectUrl)}";
-
-            System.Diagnostics.Process.Start(url);
-            var context = await http.GetContextAsync();
-            var response = context.Response;
-            var responseString = "<html><head><meta http-equiv=\'refresh\' content=\'10;url=https://google.com\'></head><body>Please return to the app.</body></html>";
-            var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-            response.ContentLength64 = buffer.Length;
-            var responseOutput = response.OutputStream;
-
-            await responseOutput.WriteAsync(buffer, 0, buffer.Length).ContinueWith((task) =>
-            {
-                responseOutput.Close();
-                http.Stop();
-            });
-
-            if (context.Request.QueryString.Get("error") != null)
-            {
-            }
-            if (context.Request.QueryString.Get("code") == null
-                || context.Request.QueryString.Get("state") == null)
-            {
-            }
-
-            // extracts the code
-            var code = context.Request.QueryString.Get("code");
-
-            return await Task.FromResult(code);
-        }
-
-        private async Task<Token> GetToken(string code)
-        {
-            var tokenRequestURI = "https://api.medium.com/v1/tokens";
-            var tokenRequestBody =
-                $"code={code}&client_id={_clientId}&client_secret={_clientSecret}&grant_type=authorization_code&redirect_uri={System.Uri.EscapeDataString(_redirectUrl)}";
-
-            var tokenRequest = (HttpWebRequest)WebRequest.Create(tokenRequestURI);
-            tokenRequest.Method = "POST";
-            tokenRequest.ContentType = "application/x-www-form-urlencoded";
-            tokenRequest.Accept = "Accept=text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
-            var _byteVersion = Encoding.ASCII.GetBytes(tokenRequestBody);
-            tokenRequest.ContentLength = _byteVersion.Length;
-            var stream = tokenRequest.GetRequestStream();
-            await stream.WriteAsync(_byteVersion, 0, _byteVersion.Length);
-            stream.Close();
-
-            var tokenResponse = await tokenRequest.GetResponseAsync();
-
-            Token token;
-
-            using (var reader = new StreamReader(tokenResponse.GetResponseStream()))
-            {
-                var responseText = await reader.ReadToEndAsync();
-
-                token = JsonConvert.DeserializeObject<Token>(responseText);
-            }
-
-            return await Task.FromResult(token);
         }
     }
 }
