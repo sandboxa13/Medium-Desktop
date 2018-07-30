3bd4c88c712a93b0c77a965c26d16b3ce64b2cd4
Dmitriy <dima_nikishin_84@mail.ru>
2018-07-30 10:14:43 +0300

    move viewmodles folder from Medium.Core -> Medium

---
+++ src/Medium/ViewModels/LoginViewModel.cs
@@ -0,0 +1,40 @@
+﻿using DryIocAttributes;
+using Medium.Core.Managers.Interfaces;
+using Medium.Core.Services;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace Medium.ViewModels
+{
+    [Reuse(ReuseType.Transient)]
+    [ExportEx(typeof(LoginViewModel))]
+    public sealed class LoginViewModel : BaseViewModel
+    {
+        public LoginViewModel(
+            ILoginManager loginManager,
+            INavigationService navigationService,
+            IMainWindowService mainWindowService)
+        {
+            LoginCommand = ReactiveCommand.CreateFromTask(async () =>
+            {
+                SpinnerVisible = true;
+
+                var result = await loginManager.LoginAsync();
+
+                if (result)
+                {
+                    mainWindowService.ActivateWindow();
+                    await navigationService.NavigateAsync<MainPageViewModel>();
+
+                    SpinnerVisible = false;
+                }
+            });
+        }
+            
+        public ReactiveCommand LoginCommand { get; }
+
+        [Reactive] public bool SpinnerVisible { get; set; }
+
+        [Reactive] public string TestText { get; set; }
+    }
+}
---
+++ src/Medium/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using DryIocAttributes;
+using Medium.Core.MediumAPI;
+using Medium.Core.Services;
+
+namespace Medium.ViewModels
+{
+    [Reuse(ReuseType.Singleton)]
+    [ExportEx(typeof(MainPageViewModel))]
+    public sealed class MainPageViewModel : BaseViewModel
+    {
+        private readonly IApiController _apiController;
+        private readonly INavigationService _navigationService;
+
+        public MainPageViewModel(IApiController apiController, INavigationService navigationService)
+        {
+            _apiController = apiController;
+            _navigationService = navigationService;
+
+        }
+    }
+}
