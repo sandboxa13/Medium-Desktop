15543566086bbfe398b7b5d2f8069cc2417ba0fe
Dmitriy <dima_nikishin_84@mail.ru>
2018-08-09 22:55:23 +0300

    add come tests

--- tests/Medium.Services.Tests/Services/ConfigurationServiceTests.cs
+++ tests/Medium.Services.Tests/Services/ConfigurationServiceTests.cs
@@ -1,9 +1,7 @@
-﻿using Moq;
-using Services.Interfaces.Interfaces;
+﻿using System.Threading.Tasks;
 using Xunit;
 using FluentAssertions;
 using Services.Impl;
-
 
 namespace Medium.Services.Tests.Services
 {
@@ -12,11 +10,22 @@ namespace Medium.Services.Tests.Services
         [Fact]  
         public void Method_Set_Base_Path_Should_Set_Path_To_Specific_Directory()
         {
-            var mockConfigurationService = /*new Mock<IConfigurationService>().Object;*/ new ConfigurationService();
+            var configurationService = new ConfigurationService();  
 
-            mockConfigurationService.SetBasePath("basePath");
+            configurationService.SetBasePath("basePath");
 
-            mockConfigurationService.BasePath.Should().Be("basePath");
+            configurationService.BasePath.Should().Be("basePath");
+        }   
+
+        [Fact]
+        public async Task Method_AddJSON_File_Should_Find_File_And_Deserialize()
+        {   
+            var configurationService = new ConfigurationService();
+
+            configurationService.SetBasePath("../../");
+            await configurationService.AddJsonFile("appsettings.json");
+
+            configurationService.GetValue<string>("ClientID").Should().NotBeNullOrEmpty();
         }
     }
 }
