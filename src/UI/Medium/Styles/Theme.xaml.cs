b6987f11770fdc2eb128b1f82cf686e8fc4c9a4f
Dmitriy <dima_nikishin_84@mail.ru>
2018-11-01 18:22:40 +0300

    add common style for textblock

---
+++ src/UI/Medium/Styles/Theme.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Medium.Styles     
+{       
+    public class Theme : UserControl
+    {
+        public Theme()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
