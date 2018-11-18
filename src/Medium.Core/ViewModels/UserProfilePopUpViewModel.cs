448fb382f8d1833f6a419525db8dc20e988e8324
Dmitriy <dima_nikishin_84@mail.ru>
2018-11-18 16:43:01 +0300

    add userProfilePopUpVM

---
+++ src/Medium.Core/ViewModels/UserProfilePopUpViewModel.cs
@@ -0,0 +1,22 @@
+﻿using DryIocAttributes;
+using ReactiveUI;
+
+namespace Medium.Core.ViewModels
+{
+    [Reuse(ReuseType.Transient)]
+    [ExportEx(typeof(UserProfilePopUpViewModel))]
+    public class UserProfilePopUpViewModel : ReactiveObject, ISupportsActivation
+    {
+        public ViewModelActivator Activator { get; }
+
+        public UserProfilePopUpViewModel()
+        {
+            Activator = new ViewModelActivator();
+
+            //this.WhenActivated(disposable =>
+            //{
+
+            //});
+        }
+    }
+}
